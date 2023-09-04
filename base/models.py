89b211f671b43a04a81656836f63c2651345aa87
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-09-04 23:55:32 +0530

    login and reg

--- base/models.py
+++ base/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 # Create your models here.
 
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete = models.CASCADE, null = True, blank=True)
     title = models.CharField(max_length=200)
+    # completiondate = models.DateField(default=datetime.date.today)
+    # remainingdays = completiondate - datetime.date.today()
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     create = models.DateField( auto_now_add=True)
 
     def __str__(self):
         return self.title
+    
+    # def calcdate():
+    #     remainingdays = completiondate - datetime.date.today() 
 
     class Meta:
         ordering = ['complete']
--- base/views.py
+++ base/views.py
@@ -1,15 +1,13 @@
-from typing import Any, Dict
-from django.forms.models import BaseModelForm
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 from django.contrib.auth.views import LoginView
-
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Task
 
@@ -21,15 +19,36 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
+class RegisterPage(FormView):
+    template_name = "base/register.html"
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get()
+    
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
+
+     #for count you have specifiy in get_context_data
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False).count()
-        return context
+        context['count'] = self.model.objects.filter(complete=False).count()
+        return context  
 
 class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
@@ -41,7 +60,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
     fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
-    def form_invalid(self, form):
+    def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreate, self).form_valid(form)
 
