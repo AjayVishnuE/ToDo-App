23f53bd182d6b45ca3a6063aa75c91d6e7675b6c
Ajay Vishnu E <ajayvishnu121@gmail.com>
2023-09-09 22:06:25 +0530

    deploy railway

--- YT_ToDo/settings.py
+++ YT_ToDo/settings.py
@@ -123,7 +123,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
